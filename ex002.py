150e3947abad001eb918cedebada875317c1cebf
Victor-Felicio <victor.adam.tankian@gmail.com>
2026-03-10 10:02:13 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,5 @@
+estoque_inicial = float(input("Digite o estoque inicial: "))
+estoque_final = float(input("Digite o estoque final: "))
+estoque_medio = (estoque_inicial + estoque_final) // 2
+
+print("O estoque médio da peça é:", estoque_medio,".")
