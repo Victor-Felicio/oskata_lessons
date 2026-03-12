8c8b6dc2d2955f700a9997f0265d469ca2457062
Victor-Felicio <victor.adam.tankian@gmail.com>
2026-03-12 08:00:13 -0300

    Chris Exercício 005

---
+++ ex005.py
@@ -0,0 +1,18 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+num4 = int(input("Digite o quarto número: "))
+
+quad1 = num1 ** 2
+quad2 = num2 ** 2
+quad3 = num3 ** 2
+quad4 = num4 ** 2
+
+soma = quad1 + quad2 + quad3 + quad4
+
+print("O quadrado do primeiro número é: ", quad1)
+print("O quadrado do segundo número é: ", quad2)
+print("O quadrado do terceiro número é: ", quad3)
+print("O quadrado do quarto número é: ", quad4)
+
+print("A soma dos quadrados é: ", soma)
